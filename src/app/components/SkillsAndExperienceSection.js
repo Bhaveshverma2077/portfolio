38f660e25ee2245602a7f58c2b21d879be26d2c5
Bhavesh Verma <bhaveshverma2077@gmail.com>
2024-03-01 22:42:35 +0530

    add skillsAndExperience section

--- src/app/components/SkillsAndExperienceSection.js
+++ src/app/components/SkillsAndExperienceSection.js
@@ -1,5 +1,14 @@
+import Skills from "./Skills";
+
 const SkillsAndExperienceSection = () => {
-  return <div className="p-12">working...</div>;
+  return (
+    <div className="p-12">
+      <p className="text-K-ASCENT text-lg tracking-[2px] pb-8">{"SKILLS"}</p>
+      <div className=" flex flex-wrap">
+        <Skills />
+      </div>
+    </div>
+  );
 };
 
 export default SkillsAndExperienceSection;
