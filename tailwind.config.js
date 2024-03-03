6ded9c52a24abf551ac36067a1b64d170ceba338
Bhavesh Verma <bhaveshverma2077@gmail.com>
2024-03-03 23:56:31 +0530

    fix-bug: dependency issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,21 @@ module.exports = {
         "K-ASCENT": "#31ffa6",
         "K-GREY-DARK": "#1f1f1f",
         "K-GREY-LIGHT": "#5f5f5f",
+        "K-BLACK": "#252525",
       },
+      // keyframes: {
+      //   wiggle: {
+      //     "0%": { transform: "translate(0,0) rotate(0deg)" },
+      //     // "10%": { transform: "translate(150px,0) rotate(0deg)"},
+      //     "50%": { transform: "translate(500px,500px) rotate(-45deg)" },
+      //     "100%": {
+      //       transform: "translate(500px,500px) rotate(-45deg)",
+      //     },
+      //   },
+      // },
+      // animation: {
+      //   wiggle: "wiggle 1s linear infinite",
+      // },
     },
   },
   plugins: [],
