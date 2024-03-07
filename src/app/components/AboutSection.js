883810080d8ccb48b19a1e810586b8171a2b5afb
Bhavesh Verma <bhaveshverma2077@gmail.com>
2024-03-07 22:23:43 +0530

    add animation in modal and add contact info

--- src/app/components/AboutSection.js
+++ src/app/components/AboutSection.js
@@ -3,28 +3,26 @@ import TalkButton from "./TalkButton";
 import Modal from "./Modal";
 import AboutSectionMainAnimation from "./AboutSectionMainAnimation";
 import SocialLinks from "./SocialLinks";
-import { useRouter, useSearchParams } from "next/navigation";
 
 const AboutSection = () => {
-  const searchParams = useSearchParams();
-
-  const showModal = searchParams.get("modal")?.toLowerCase();
   return (
-    <div className="relative m-12">
-      <TalkButton></TalkButton>
-      {showModal === "true" && <Modal></Modal>}
-      <AboutSectionMainAnimation />
-      <DecoratedProfileImage></DecoratedProfileImage>
-      <p className="text-5xl text-K-GREY-LIGHT">
-        HELLO THERE<span className="text-K-ASCENT">,</span>
-      </p>
-      <p className="text-3xl pb-4">MY NAME IS BHAVESH VERMA</p>
-      <p className="text-6xl w-[60rem] pb-4">
-        <span className="text-K-GREY-DARK"> I AM A</span>
-        <br />
-        FULL STACK DEVELOPER<span className="text-K-ASCENT">.</span>
-      </p>
-      <SocialLinks></SocialLinks>
+    <div className="overflow-hidden h-full w-full">
+      <div className="relative m-12 animate-fade_translate">
+        <TalkButton></TalkButton>
+        <Modal></Modal>
+        <AboutSectionMainAnimation />
+        <DecoratedProfileImage></DecoratedProfileImage>
+        <p className="text-5xl text-K-GREY-LIGHT">
+          HELLO THERE<span className="text-K-ASCENT">,</span>
+        </p>
+        <p className="text-3xl pb-4">MY NAME IS BHAVESH VERMA</p>
+        <p className="text-6xl w-[60rem] pb-4">
+          <span className="text-K-GREY-DARK"> I AM A</span>
+          <br />
+          FULL STACK DEVELOPER<span className="text-K-ASCENT">.</span>
+        </p>
+        <SocialLinks></SocialLinks>
+      </div>
     </div>
   );
 };
--- src/app/components/Modal.js
+++ src/app/components/Modal.js
@@ -2,9 +2,20 @@ import Link from "next/link";
 import CloseIcon from "./icons/CloseIcon";
 import DocumentIcon from "./icons/DocumentIcon";
 
+import { useSearchParams } from "next/navigation";
+
 const Modal = () => {
+  const searchParams = useSearchParams();
+  console.log(searchParams.get("modal"));
+  const showModal = searchParams.get("modal")?.toLowerCase();
   return (
-    <div className="absolute z-10 bg-black top-4 right-0 border-K-ASCENT border-2 p-6">
+    <div
+      className={`${
+        showModal === "true"
+          ? "opacity-100 translate-y-0 visible"
+          : "opacity-0 translate-y-6 invisible"
+      } transition-[opacity,visibility,transform] duration-500 absolute z-10 bg-black top-0 right-0 border-K-ASCENT border-2 p-6`}
+    >
       <Link
         href={"?modal=false"}
         className="transition-transform hover:scale-[1.1] hover:text-black hover:bg-K-ASCENT absolute bg-black -left-4 -top-4 border-2 border-K-ASCENT h-10 w-10 flex items-center justify-center"
@@ -20,7 +31,11 @@ const Modal = () => {
           appealing but also highly interactive, delivering a seamless
           experience. I hope my work resonates with you.
         </p>
-        <div className="flex gap-2 hover:gap-3 transition-[gap] items-center hover:bg-K-GREY-DARK py-1 px-2">
+        <p className="text-[0.8rem] py-2">
+          Contact Me:{" "}
+          <span className="text-K-ASCENT">bhaveshverma2077@gmail.com</span>
+        </p>
+        <div className="cursor-pointer flex gap-2 hover:gap-3 transition-[gap,padding] items-center hover:bg-K-GREY-DARK py-1 hover:px-2">
           <p className="text-K-ASCENT text-[0.8rem]">RESUME</p>
           <DocumentIcon></DocumentIcon>
         </div>
