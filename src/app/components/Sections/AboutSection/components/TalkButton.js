597e9f687a54da43e63649de5f2cc40129e49d86
Bhavesh Verma <bhaveshverma2077@gmail.com>
2024-03-30 22:53:40 +0530

    update talk button

--- src/app/components/Sections/AboutSection/components/TalkButton.js
+++ src/app/components/Sections/AboutSection/components/TalkButton.js
@@ -5,7 +5,7 @@ const TalkButton = () => {
   return (
     <Link
       href={"?modal=true"}
-      className="absolute z-10 hover:bg-K-ASCENT hover:text-black transition-colors duration-300 p-4 rounded-tr-3xl top-0 right-0 flex gap-4 items-center"
+      className={`hidden md:flex absolute z-10 hover:bg-K-ASCENT hover:text-black transition-colors duration-300 p-4 rounded-tr-3xl top-0 right-0 gap-4 items-center`}
     >
       <p className="font-normal">Let's Talk</p>
       <div className="bg-K-ASCENT w-8 h-8 rounded-full flex justify-center items-center">
