ee1ef045e59ee9db730cad69f6a358e32207c974
Bhavesh Verma <bhaveshverma2077@gmail.com>
2024-03-31 23:32:02 +0530

    update project tile

--- src/app/components/Sections/ProjectsSection/components/ProjectTile.js
+++ src/app/components/Sections/ProjectsSection/components/ProjectTile.js
@@ -1,18 +1,25 @@
 import Link from "next/link";
 import CheronRightIcon from "../../../icons/ChevronRightIcon";
 import GithubIcon from "../../../icons/GithubIcon";
+import Image from "next/image";
 
-const ProjectTile = ({ name, githubRepoLink, deployLink }) => {
+const ProjectTile = ({ name, githubRepoLink, deployLink, imageUrl }) => {
   return (
-    <div className="w-80 flex flex-col gap-2">
-      <div className="border border-K-GREY-LIGHT w-full h-64"></div>
+    <div className="w-full md:w-80 flex flex-col gap-2">
+      <div className="border border-K-GREY-LIGHT w-full h-64 relative overflow-hidden">
+        <Image
+          className="object-cover origin-left hover:scale-110 transition-transform"
+          src={imageUrl}
+          fill
+        ></Image>
+      </div>
       <div className="flex gap-2">
         <div className="border border-K-GREY-LIGHT w-1/2 h-12 flex items-center justify-center">
           <p>{name.toUpperCase()}</p>
         </div>
         <Link
           href={githubRepoLink}
-          className="border border-K-GREY-LIGHT w-1/4 h-12 flex items-center justify-center"
+          className="hover:bg-K-GREY-DARK border border-K-GREY-LIGHT w-1/4 h-12 flex items-center justify-center"
         >
           <div className="scale-[0.3] ">
             <GithubIcon></GithubIcon>
@@ -20,7 +27,7 @@ const ProjectTile = ({ name, githubRepoLink, deployLink }) => {
         </Link>
         <Link
           href={deployLink}
-          className="border border-K-GREY-LIGHT w-1/4 h-12 flex items-center justify-center"
+          className="hover:bg-K-GREY-DARK border border-K-GREY-LIGHT w-1/4 h-12 flex items-center justify-center"
         >
           <CheronRightIcon></CheronRightIcon>
         </Link>
