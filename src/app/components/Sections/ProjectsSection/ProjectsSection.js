311f20096f91db37aeb53abf7c5941d31af0f6e5
Bhavesh Verma <bhaveshverma2077@gmail.com>
2024-04-03 21:42:59 +0530

    update projects section

--- src/app/components/Sections/ProjectsSection/ProjectsSection.js
+++ src/app/components/Sections/ProjectsSection/ProjectsSection.js
@@ -2,11 +2,24 @@ import ProjectTile from "./components/ProjectTile";
 
 const ProjectsSection = () => {
   return (
-    <div className="p-12 text-[0.8rem] w-full font-normal flex flex-wrap overflow-x-auto h-full gap-2">
+    <div className="p-6 md:p-12 text-[0.8rem] w-full font-normal flex flex-wrap overflow-x-auto h-full gap-2 custom_scrollbar">
       <ProjectTile
-        name={"project name"}
+        name={"movies app"}
+        imageUrl={"/images/projects/movies_app.png"}
         deployLink={"/something"}
-        githubRepoLink={"/some%20other%20thing"}
+        githubRepoLink={"https://github.com/Bhaveshverma2077/Movies-App"}
+      ></ProjectTile>
+      <ProjectTile
+        name={"shopping app"}
+        imageUrl={"/images/projects/shopping_app.png"}
+        deployLink={"/something"}
+        githubRepoLink={"https://github.com/Bhaveshverma2077/shopping-app"}
+      ></ProjectTile>
+      <ProjectTile
+        name={"portfolio"}
+        imageUrl={"/images/projects/portfolio.png"}
+        deployLink={"/something"}
+        githubRepoLink={"https://github.com/Bhaveshverma2077/portfolio"}
       ></ProjectTile>
     </div>
   );
