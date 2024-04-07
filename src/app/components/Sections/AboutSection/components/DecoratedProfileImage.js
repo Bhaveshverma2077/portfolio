06aeba5ec9ec70ab8cfdf112e7a24e205c72bbbf
Bhavesh Verma <bhaveshverma2077@gmail.com>
2024-04-07 15:08:48 +0530

    update photo

--- src/app/components/Sections/AboutSection/components/DecoratedProfileImage.js
+++ src/app/components/Sections/AboutSection/components/DecoratedProfileImage.js
@@ -1,8 +1,7 @@
 import Image from "next/image";
 import DecoratedLines from "./DecoratedLines";
 
-const IMGURL =
-  "https://images.unsplash.com/photo-1707343848723-bd87dea7b118?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+const IMGURL = "images/my_photo.jpeg";
 
 const DecoratedProfileImage = () => {
   return (
